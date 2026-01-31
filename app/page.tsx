5f92b377cede36b2acc240c67d7e2419ef071c5e
joe molina <joedodaniljr123@gmail.com>
2026-01-31 19:26:41 -0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,65 @@
-import Image from "next/image";
+'use client'
+
+import { Landmark } from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
+  const [divisa, setDivisa] = useState(0)
+  const [porcentaje, setPorcentaje] = useState(0)
+  const [bs, setBs] = useState(0)
+
+  const totalBinance = (divisa - (divisa*(porcentaje/100))) - 0.06
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col min-h-screen font-sans bg-blue-950/10">
+      <div className="h-16 font-bold text-xl flex items-center px-2  bg-blue-900/10 border-y border-gray-800/50 w-full">
+        DolarInteligente
+      </div>
+      <div className="flex flex-col gap-5  p-3">
+        <div className="opacity-50 font-normal text-sm">
+          Tu guía estratégica para la venta de divisas. Calcula el precio mínimo ideal para vender tu efectivo sin quedar por debajo de la tasa USDT. Una referencia clara y rápida para proteger tu capital y negociar siempre con los mejores márgenes del mercado.
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="flex flex-col flex-wrap bg-blue-900/10 p-3 rounded-md  gap-3">
+          <span className="flex items-center"><Landmark className="bg-blue-950 text-blue-500 rounded-md p-1 mr-2"/><p className="text-sm font-thin opacity-50">Datos de venta</p></span>
+
+        <div className="flex flex-wrap gap-4">
+                    <div className="flex flex-col">
+            <p className="text-xs opacity-50">cantidad en divisa</p>
+            
+            <div className="flex items-center text-3xl font-black"><input type="num" className="field-sizing-content min-w-3" onChange={(e) => setDivisa(Number(e.target.value))} placeholder="ej: 100" /><p className="font-normal opacity-50"> $</p></div>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="text-xs opacity-50">% de cambio</p>
+            
+            <div className="flex items-center text-3xl font-black"><input type="num" className="field-sizing-content min-w-3" onChange={(e) => setPorcentaje(Number(e.target.value))} placeholder="por defecto 0" /><p className="font-normal opacity-50">%</p></div>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="text-xs opacity-50">tasa de cambio USDT x Bs</p>
+            
+            <div className="flex items-center text-3xl font-black"><input type="num" className="field-sizing-content min-w-3" onChange={(e) => setBs(Number(e.target.value))} placeholder="ej: 510" /><p className="font-normal opacity-50">Bs</p></div>
+          </div>
         </div>
-      </main>
+
+
+        </div>
+        { divisa >0 && bs > 0  && <div className="flex flex-col bg-linear-to-r from-blue-300/10 to-blue-300/5 p-3 rounded-md  gap-5">
+        <div className="flex justify-between opacity-70 text-sm font-thin"><p>Bs total despues de venta</p><p>Precio por cada $</p></div>
+
+          <div className="flex flex-col">
+            <div className="flex justify-center gap-2 text-3xl text-orange-400">
+              <div className="flex items-center  font-bold">{(totalBinance*bs).toFixed(2)}<p>Bs</p></div>
+            /
+            <div className="flex items-center font-bold">{((totalBinance*bs)/divisa).toFixed(2)}<p >Bs</p></div>
+            </div>
+          <div className="flex justify-center opacity-70 text-sm font-thin"> vendidos: {totalBinance.toFixed(2)} USDT</div>
+          </div>
+          <hr className="opacity-10"/>
+          <p className="opacity-70 text-sm font-thin text-center">Vende en físico solo si el precio es mayor a {((totalBinance*bs)/divisa).toFixed(2)} Bs Si te ofrecen menos, obtendrás mayor beneficio convirtiendo tus divisas a USDT.</p>
+        </div>}
+      </div>
+      
     </div>
   );
 }
